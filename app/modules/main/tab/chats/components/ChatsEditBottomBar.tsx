c01856176aa734373c9a9c60387cf2c1064908f2
Prakash Kumar Mandal <prakash.mandal@binaryinformatics.com>
2021-02-24 07:19:59 +0530

    bottom edit action bar animtion done

---
+++ app/modules/main/tab/chats/components/ChatsEditBottomBar.tsx
@@ -0,0 +1,40 @@
+import React, { useRef, useImperativeHandle, forwardRef } from 'react'
+import { View, Text, Animated } from 'react-native';
+
+const ChatsEditBottomBar = (props: any, ref: any) => {
+    const transitionValue = {
+        hide: 150,
+        show: 0
+    }
+
+    const animatedViewRef = useRef(new Animated.Value(0)).current;
+
+    const transitionY = animatedViewRef.interpolate({
+        inputRange: [0, 1],
+        outputRange: [transitionValue.hide, transitionValue.show],
+    });
+
+    const toggleActionBar = (isHide: boolean) => {
+        Animated.timing(animatedViewRef, {
+            toValue: isHide ? 0 : 1,
+            duration: 200,
+            useNativeDriver: true,
+        }).start();
+    }
+
+    useImperativeHandle(ref, () => (
+        {
+            toggleActionBar: (value: boolean) => toggleActionBar(value)
+        }
+    ));
+
+    return (
+        <Animated.View style={{
+            transform: [{ translateY: transitionY }]
+        }}>
+            <Text>sdfsdfsdfsdfsdfsdfdsf</Text>
+        </Animated.View>
+    )
+}
+
+export default forwardRef(ChatsEditBottomBar);
--- app/modules/main/tab/chats/index.tsx
+++ app/modules/main/tab/chats/index.tsx
@@ -1,28 +1,38 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import { View, Text } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import Device from 'react-native-device-detection';
 import { useDispatch, useSelector } from 'react-redux';
 import { TouchableHighlight } from 'react-native-gesture-handler';
 import toggleTabVisibility from '@app/store/actions/tab-visibility.action';
+import ChatsEditBottomBar from './components/ChatsEditBottomBar';
 interface ChatsScreenProps {
 
 }
 
 export const ChatsScreen: React.FC = (props: ChatsScreenProps)=> {
     const dispatch = useDispatch();
+    const chatsEditBottomBarRef = useRef<any>(null);
     const { networkConnection }  = useSelector((state: any) => state);
+
+    
+    const toggleActionBar = (value: boolean) => {
+        chatsEditBottomBarRef.current.toggleActionBar(value);
+    }
     return (
-        <SafeAreaView>
+        <SafeAreaView style={{flex: 1, justifyContent: 'space-between'}}>
+            <View>
             <Text>sdfsfsdf{JSON.stringify(Device.isTablet)}</Text>
             <Text>sdfsfsdf{JSON.stringify(Device.isIphoneX)}</Text>
             <Text>{!networkConnection.isConnected ? 'Waiting for network...' : null}</Text>
-            <TouchableHighlight onPress={() =>  dispatch(toggleTabVisibility(false))} >
+            <TouchableHighlight onPress={() =>  {dispatch(toggleTabVisibility(false)); toggleActionBar(false)}} >
                 <Text>Hide tab</Text>
             </TouchableHighlight>
-            <TouchableHighlight onPress={() =>  dispatch(toggleTabVisibility(true))} >
+            <TouchableHighlight onPress={() =>  {dispatch(toggleTabVisibility(true)); toggleActionBar(true)}} >
                 <Text>Show Tab</Text>
             </TouchableHighlight>
+            </View>
+            <ChatsEditBottomBar ref={chatsEditBottomBarRef}/>
         </SafeAreaView>
     )
 }
