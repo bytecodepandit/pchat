097e00ece95ab0ec82a3d7d95ed34546cfe49d67
Prakash Kumar Mandal <prakash.mandal@binaryinformatics.com>
2021-03-15 00:06:58 +0530

    camera setup done

--- app/modules/main/tab/camera/index.tsx
+++ app/modules/main/tab/camera/index.tsx
@@ -1,15 +1,47 @@
-import React from 'react'
-import { View, Text } from 'react-native'
-import { SafeAreaView } from 'react-native-safe-area-context'
+import React, { useEffect, useRef, useState } from 'react'
+import { View, Text, TouchableOpacity } from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { RNCamera } from 'react-native-camera';
+import {launchImageLibrary} from 'react-native-image-picker';
 
 interface CameraScreenProps {
 
 }
 
-export const CameraScreen: React.FC = (props: CameraScreenProps)=> {
+export const CameraScreen: React.FC = (props: CameraScreenProps) => {
+    const cameraRef = useRef<any>(null);
+    const [isFlashMode, setIsFlashMode] = useState<'on' | 'off'>('on');
+    const [isBackCamera, setIsBackCamera] = useState<'back' | 'front'>('back');
+    const takePicture = async () => {
+        try {
+            const data = await cameraRef.current.takePictureAsync();
+            console.log('Path to image: ' + data.uri);
+        } catch (err) {
+            // console.log('err: ', err);
+        }
+    };
+
     return (
-        <SafeAreaView>
-            <Text>Camera</Text>
+        <SafeAreaView style={{ flex: 1, backgroundColor: 'yellow' }}>
+            <RNCamera
+                ref={cameraRef}
+                captureAudio={false}
+                type={RNCamera.Constants.Type[isBackCamera]}
+                flashMode={RNCamera.Constants.FlashMode[isFlashMode]}
+                style={{ flex: 1 }}
+            >
+                <View >
+                    <TouchableOpacity onPress={takePicture}>
+                        <Text style={{ color: '#fff' }}>Take Photo</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity onPress={() => setIsFlashMode('off')}>
+                        <Text style={{ color: '#fff' }}>on flash</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity onPress={() => launchImageLibrary({quality: 0.5, mediaType: 'photo'}, (event) => console.log('sdfsdfsdf', event))}>
+                        <Text style={{ color: '#fff' }}>Open Gallery</Text>
+                    </TouchableOpacity>
+                </View>
+            </RNCamera>
         </SafeAreaView>
     )
 }
