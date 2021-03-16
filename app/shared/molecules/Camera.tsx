b068bb57e369c571f3b1c0b77f7971cbd5f84a75
Prakash Kumar Mandal <prakash.mandal@binaryinformatics.com>
2021-03-16 10:02:07 +0530

    camera image thumbnail UI init

--- app/shared/molecules/Camera.tsx
+++ app/shared/molecules/Camera.tsx
@@ -1,5 +1,5 @@
 import React, { useCallback, useEffect, useRef, useState } from 'react';
-import { View, TouchableOpacity, GestureResponderEvent } from 'react-native';
+import { View, TouchableOpacity, GestureResponderEvent, FlatList, Image } from 'react-native';
 import Orientation from 'react-native-orientation';
 import { RNCamera } from 'react-native-camera';
 import { ImagePickerResponse, launchImageLibrary } from 'react-native-image-picker';
@@ -11,9 +11,12 @@ import AntDesign from 'react-native-vector-icons/AntDesign';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
 import Entypo from 'react-native-vector-icons/Entypo';
-import { TouchableWithoutFeedback } from 'react-native-gesture-handler';
+import { ScrollView, TouchableWithoutFeedback } from 'react-native-gesture-handler';
 import { colors } from 'react-native-elements';
 import { RFValue } from 'react-native-responsive-fontsize';
+import RNFetchBlob from 'rn-fetch-blob';
+import { moderateScale } from 'react-native-size-matters';
+import GestureRecognizer from 'react-native-swipe-gestures';
 
 interface CameraScreenProps {
     close: (event?: GestureResponderEvent) => void;
@@ -21,22 +24,37 @@ interface CameraScreenProps {
     selectPhoto: (event?: ImagePickerResponse) => void;
 }
 
+var TRACK_FOLDER = RNFetchBlob.fs.dirs.LibraryDir;
+
 const Camera = ({ close, capture, selectPhoto }: CameraScreenProps) => {
     const isFocused = useIsFocused()
     const cameraRef = useRef<any>(null);
     const [isFlashMode, setIsFlashMode] = useState<boolean>(true);
     const [isBackCamera, setIsBackCamera] = useState<boolean>(true);
+    const [localStoredImages, SetLocalStoredImages] = useState<string[]>([]);
     const dispatch = useDispatch();
+    const images = [
+        'https://images.sadhguru.org/sites/default/files/media_files/love-quotes_0.jpg',
+        'https://cdn11.bigcommerce.com/s-x49po/images/stencil/1280x1280/products/36835/50093/1547274864391_Screenshot_20190108_111037_copy__55052.1547528015.jpg?c=2',
+        'https://lh6.ggpht.com/HlgucZ0ylJAfZgusynnUwxNIgIp5htNhShF559x3dRXiuy_UdP3UQVLYW6c=s1200',
+        'https://easttennessean.com/wp-content/uploads/2018/04/art.jpg',
+        'https://thumbs.dreamstime.com/b/racial-love-white-caucasion-black-african-american-hands-shaped-as-interracial-heart-representing-world-unity-ethnic-162292478.jpg',
+        'https://www.lovethispic.com/uploaded_images/348511-I-Love-U.jpg',
+        'https://www.trueshayari.in/wp-content/uploads/2018/07/Cute-Love-Images-in-English.jpg',
+        'https://i.pinimg.com/originals/2f/9d/95/2f9d9562eb2252ae132b4bf8258aa18a.jpg'
+    ]
 
     useEffect(() => {
+        SetLocalStoredImages(images);
         if (isFocused) {
             dispatch(toggleStatusBar(true));
             dispatch(toggleTabVisibility(false));
-            Orientation.addSpecificOrientationListener((event) => {
-                
-            })
+            RNFetchBlob.fs.ls(TRACK_FOLDER + '/Caches/Camera').then(files => {
+                console.log(TRACK_FOLDER + '/Caches/Camera', files);
+
+            }).catch(error => console.log(error));
         }
-    }, [isFocused])
+    }, [isFocused]);
 
     useFocusEffect(
         useCallback(() => {
@@ -47,10 +65,27 @@ const Camera = ({ close, capture, selectPhoto }: CameraScreenProps) => {
         }, [])
     );
 
+
+
+    const saveFileToPChat = async (filePath: string, newFilepath: string) => {
+        return new Promise((resolve, reject) => {
+            RNFetchBlob.fs.mv(filePath, newFilepath)
+                .then(() => {
+                    console.log('FILE MOVED', filePath, newFilepath);
+                    resolve(true);
+                })
+                .catch(error => {
+                    console.log('moveFile error', error);
+                    reject(error);
+                });
+        });
+    };
+
     const takePicture = async () => {
         try {
             const data = await cameraRef.current.takePictureAsync();
             console.log('Path to image: ' + data.uri);
+            const result = await saveFileToPChat(data.uri, TRACK_FOLDER + '/pchat')
             capture(data);
         } catch (err) {
             // console.log('err: ', err);
@@ -108,6 +143,17 @@ const Camera = ({ close, capture, selectPhoto }: CameraScreenProps) => {
         </Box>
     }
 
+    const _renderSharedImageVideos = () => {
+        return localStoredImages.length > 0 ? <FlatList
+        horizontal={true}
+        data={localStoredImages}
+        showsHorizontalScrollIndicator={false}
+        renderItem={({ item, index }) => <Box paddingRight="hxs" key={`thum_image_${index}`}>
+            <Image style={{ width: moderateScale(80), height: moderateScale(80), opacity: 0.85 }} source={{ uri: item }} />
+        </Box>}
+    /> : null
+    }
+
     return (
         <RNCamera
             ref={cameraRef}
@@ -118,7 +164,10 @@ const Camera = ({ close, capture, selectPhoto }: CameraScreenProps) => {
             onDoubleTap={() => setIsBackCamera(!isBackCamera)}
         >
             {_renderHeader()}
-            {_renderFooter()}   
+            <View>
+                {_renderSharedImageVideos()}
+                {_renderFooter()}
+            </View>
         </RNCamera>
     )
 }
--- app/theme/spacing.ts
+++ app/theme/spacing.ts
@@ -2,14 +2,17 @@ import { moderateScale, scale, verticalScale } from 'react-native-size-matters';
 import palette from './palette';
 
 export default {
+    xs: moderateScale(5),
     s: moderateScale(10),
     m: moderateScale(15),
     l: moderateScale(24),
     xl: moderateScale(40),
+    vxs: verticalScale(5),
     vs: verticalScale(10),
     vm: verticalScale(15),
     vl: verticalScale(24),
     vxl: verticalScale(40),
+    hxs: scale(5),
     hs: scale(10),
     hm: scale(15),
     hl: scale(24),
